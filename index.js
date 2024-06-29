3c6a1dd811adf90e224324b1001e7850a2e97873
G.Tarun <119655059+gtarun8341@users.noreply.github.com>
2024-06-29 14:57:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const webhook = require('./api/webhook');
 
+// Middleware to parse JSON bodies
 app.use(express.json());
+
+// Route to handle webhook endpoint
 app.use('/api/webhook', webhook);
 
+// Define the port where the server will listen
 const PORT = process.env.PORT || 3000;
+
+// Start the server and log a message once it's running
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
